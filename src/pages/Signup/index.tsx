7c048563b3f42ca658f97bf4449c89204d895e22
tbs01215 <tbs01215@gmail.com>
2024-07-06 23:58:29 +0900

    feat: BookStyleContainer 컴포넌트 생성

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import SignupForm from '../../components/auth/SignupForm';
 import AuthCover from '../../components/auth/AuthCover';
+import BookStyleContainer from '../../components/common/BookStyleContainer';
 
 export interface SignupProps {
   email: string;
@@ -10,23 +10,9 @@ export interface SignupProps {
 
 export default function Signup() {
   return (
-    <SignupStyle>
+    <BookStyleContainer>
       <AuthCover />
       <SignupForm />
-    </SignupStyle>
+    </BookStyleContainer>
   );
 }
-
-export const SignupStyle = styled.main`
-  display: flex;
-  min-height: 600px;
-  height: 80vh;
-  max-height: 800px;
-  justify-content: space-between;
-  align-items: center;
-  /* margin: 80px auto; */
-  background-color: #f9f9f9;
-  outline: 1px solid #f9f9f9;
-  border-radius: 10px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-`;
