77d4e146fb4b1198114cdf91635f544647b6eb0e
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-07-15 08:01:46 +0900

    style:gauge 조건 설정

--- src/components/common/Gauge.tsx
+++ src/components/common/Gauge.tsx
@@ -33,19 +33,24 @@ const GaugeBar = styled.div<{ $percent: number | undefined }>`
   height: 15px;
   background-color: ${COLORS.secondary};
   border-radius: 20px;
-  background-image: linear-gradient(
-    90deg,
-    rgba(103, 140, 110, 0.5) 0%,
-    rgba(138, 174, 145, 0.75) 25%,
-    #9abaa2 50%,
-    rgba(138, 174, 145, 0.75) 75%,
-    rgba(103, 140, 110, 0.5) 100%
-  );
-  background-size: 200px 100%;
-  animation: ${({ $percent }) =>
-    $percent === 100 &&
-    css`
-      ${wave} 1.5s infinite linear;
-    `};
+
+  ${({ $percent }) =>
+    $percent === 100
+      ? css`
+          background-image: linear-gradient(
+            90deg,
+            rgba(103, 140, 110, 0.5) 0%,
+            rgba(138, 174, 145, 0.75) 25%,
+            #9abaa2 50%,
+            rgba(138, 174, 145, 0.75) 75%,
+            rgba(103, 140, 110, 0.5) 100%
+          );
+          background-size: 200px 100%;
+          animation: ${wave} 1.5s infinite linear;
+        `
+      : css`
+          background: ${COLORS.gray60};
+        `}
+
   transition: width 0.3s ease;
 `;
