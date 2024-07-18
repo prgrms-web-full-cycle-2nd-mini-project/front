3b5c2a04b489c0d5cedfb75e5fe1e6d5d4fa6122
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-07-18 18:11:52 +0900

    style: 빈 일정의 경우 ui 추가

--- src/components/TripDetailModal/TripDetailRight/TripDetailScheduleList/index.tsx
+++ src/components/TripDetailModal/TripDetailRight/TripDetailScheduleList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { ISchedule } from '../../../../apis/tripDetail';
 import TripDetailScheduleItem from './TripDetailScheduleItem';
+import Typography from '../../../common/Typography';
 
 interface ITripDetailScheduleListProps {
   scheduleListData: ISchedule[];
@@ -14,6 +15,14 @@ export default function TripDetailScheduleList({
 }: ITripDetailScheduleListProps) {
   return (
     <TripDetailScheduleListStyle>
+      {scheduleListData.length === 0 && (
+        <EmptyBox>
+          <img src="/src/assets/empty.png" />
+          <Typography $variant={'title1'} $color="gray50">
+            추가된 일정이 없습니다.
+          </Typography>
+        </EmptyBox>
+      )}
       {scheduleListData.map((schedule, idx) => (
         <TripDetailScheduleItem
           idx={idx}
@@ -29,4 +38,16 @@ export default function TripDetailScheduleList({
 const TripDetailScheduleListStyle = styled.div`
   height: 100%;
   overflow-y: auto;
+`;
+
+const EmptyBox = styled.div`
+  margin-top: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  img {
+    width: 100px;
+    opacity: 0.3;
+  }
 `;
