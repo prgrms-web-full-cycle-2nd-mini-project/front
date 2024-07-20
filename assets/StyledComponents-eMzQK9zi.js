b03a1af96ffcd8612b4b967dafd11d2c3b0bb23d
tbs01215 <tbs01215@gmail.com>
2024-07-20 15:47:10 +0900

    Updates

--- assets/StyledComponents-eMzQK9zi.js
+++ assets/StyledComponents-eMzQK9zi.js
@@ -1,4 +1,4 @@
-import{o as t,p as o}from"./index-B__vCDmL.js";import{L as i}from"./index.esm-D9xjwIHO.js";import{T as r}from"./Typography-NGKREtXo.js";function m(){return t.jsxs(i,{children:[t.jsxs(s,{children:[t.jsx(a,{children:"TRIP PLAN"}),t.jsx(c,{children:"여행 투두 리스트를 만들어볼까요?"})]}),t.jsx(d,{src:"./assets/airplane.png"})]})}const s=o.div`
+import{o as t,p as o}from"./index-C6z4j6OX.js";import{L as i}from"./index.esm-B4oHGBcl.js";import{T as r}from"./Typography-CLnIW3iW.js";function m(){return t.jsxs(i,{children:[t.jsxs(s,{children:[t.jsx(a,{children:"TRIP PLAN"}),t.jsx(c,{children:"여행 투두 리스트를 만들어볼까요?"})]}),t.jsx(d,{src:"./assets/airplane.png"})]})}const s=o.div`
   text-align: center;
 `,a=o.div`
   font-size: 2rem;
